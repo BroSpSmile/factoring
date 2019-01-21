7c6d8263d4fc59833680841dd1df049a0b9f0dd0
smile.jing <jingyongxiang@gmail.com>
2019-01-21 09:49:14 +0800

    commit

---
+++ src/main/java/com/smile/start/integration/aliyun/FileClient.java
@@ -0,0 +1,14 @@
+/**
+ * jsszvip.com Inc.
+ * Copyright (c) 2012-2019 All Rights Reserved.
+ */
+package com.smile.start.integration.aliyun;
+
+/**
+ * 阿里云文件服务客户端
+ * @author smile.jing
+ * @version $Id: FileClient.java, v 0.1 Jan 18, 2019 2:27:14 PM smile.jing Exp $
+ */
+public interface FileClient {
+    
+}
