b9112e5b8b749d793b16f11b709d2ee0b644f524
xioutmanksq <xhrenzhi@163.com>
2019-02-17 16:46:07 +0800

    归档申请

--- src/main/java/com/smile/start/model/enums/FilingProgress.java
+++ src/main/java/com/smile/start/model/enums/FilingProgress.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils;
  * @version $Id: Progress.java, v 0.1 Jan 8, 2019 9:10:26 PM smile.jing Exp $
  */
 public enum FilingProgress {
-    TOBEFILED("TOBEFILED", "归档申请人"),
+    TOBEFILED("TOBEFILED", "提出归档申请"),
     FILE("FILE", "法务分控审核"),
     FILEAUDIT("FILEAUDIT", "办公室专员归档"),
     FILECOMPLETE("FILECOMPLETE", "归档完成");
