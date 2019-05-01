edd406ba262bba53580eba824f386c2527dafc4a
dys09435 <dys09435@ly.com>
2019-05-01 15:54:46 +0800

    优化合同审验驳回到提出申请时的状态变更逻辑

--- src/main/java/com/smile/start/event/listener/ContractAuditLinstener.java
+++ src/main/java/com/smile/start/event/listener/ContractAuditLinstener.java
@@ -4,11 +4,16 @@
  */
 package com.smile.start.event.listener;
 
+import com.smile.start.model.contract.ContractInfo;
+import com.smile.start.service.contract.ContractInfoService;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import com.smile.start.event.AuditEvent;
 import com.smile.start.model.enums.AuditType;
 import com.smile.start.model.project.Audit;
+
+import javax.annotation.Resource;
 
 /**
  * 
@@ -18,14 +23,22 @@ import com.smile.start.model.project.Audit;
 @Service
 public class ContractAuditLinstener implements AuditListener {
 
+    @Resource
+    private ContractInfoService contractInfoService;
+
     /** 
      * @see com.smile.start.event.listener.AuditListener#listener(com.smile.start.event.AuditEvent)
      */
     @Override
+    @EventListener
     public void listener(AuditEvent event) {
         Audit audit = event.getAudit();
         if (AuditType.DRAWUP == audit.getAuditType()) {
-
+            if(audit.getStep() == 0) {
+                ContractInfo contractInfo = contractInfoService.getBaseInfo(audit.getProject().getId());
+                contractInfo.setStatus(0);
+                contractInfoService.updateBaseInfo(contractInfo);
+            }
         }
     }
 
