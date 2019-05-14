a971cbccd2b5a4d4814c2b44c7439492bf19fcbc
dys09435 <dys09435@ly.com>
2019-05-14 21:56:27 +0800

    归档审核监控器，判断回款是否完成

--- src/main/java/com/smile/start/event/listener/FileAuditListener.java
+++ src/main/java/com/smile/start/event/listener/FileAuditListener.java
@@ -3,6 +3,12 @@ package com.smile.start.event.listener;
 import com.smile.start.dao.FilingDao;
 import com.smile.start.dao.ProjectDao;
 import com.smile.start.event.AuditEvent;
+import com.smile.start.model.contract.ContractInfo;
+import com.smile.start.model.enums.AuditType;
+import com.smile.start.model.enums.Step;
+import com.smile.start.model.project.Audit;
+import com.smile.start.model.project.Project;
+import com.smile.start.service.engine.ProcessEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
@@ -24,17 +30,8 @@ public class FileAuditListener implements AuditListener {
      */
     public Logger      logger = LoggerFactory.getLogger(getClass());
 
-    /**
-     * 项目DAO
-     */
     @Resource
-    private ProjectDao projectDao;
-
-    /**
-     * 归档DAO
-     */
-    @Resource
-    private FilingDao  filingDao;
+    private ProcessEngine processEngine;
 
     /**
      * @see com.smile.start.event.listener.AuditListener#listener(com.smile.start.event.AuditEvent)
@@ -42,8 +39,16 @@ public class FileAuditListener implements AuditListener {
     @Override
     @EventListener
     public void listener(AuditEvent event) {
-        //        
-
+        Audit audit = event.getAudit();
+        if (AuditType.FILE == audit.getAuditType()) {
+            //办公室专员审核完成，直接到完结状态
+            if(audit.getStep() == -1) {
+                //TODO 回款进度判断
+//                Project project = audit.getProject();
+//                project.setStep(Step.END.getIndex());
+//                processEngine.next(project, false);
+            }
+        }
     }
 
 }
