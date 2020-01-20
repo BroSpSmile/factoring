3e37455d4e745c499da23dcb4aa33e3c9aba1449
smile.jing <jingyongxiang@gmail.com>
2020-01-20 14:37:07 +0800

    commit

--- restart.sh
+++ restart.sh
@@ -3,5 +3,5 @@ pid=`jps|grep start|awk -F' '  '{print $1}'`
 echo $pid
 kill -9 $pid
 echo "kill $pid success"
-nohup java -jar target/start-0.0.1-SNAPSHOT.jar>out.log&
+nohup java -jar target/start-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod>out.log&
 echo "restart success"
